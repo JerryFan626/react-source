17e7b4b2f6cb8b78225f646ec77a54e0782dc35e
huanyueyaoqin <huanyueyaoqin@qq.com>
2020-03-16 10:16:59 +0800

    redux 函数组件&类组件使用

--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
-import { createStore } from "./redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import Counter from "./components/Counter";
 
-const INCREMENT = "INCREMENT";
-const DECREMENT = "DECREMENT";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case INCREMENT:
-      return { num: state.num + 1 };
-    case DECREMENT:
-      return { num: state.num - 1 };
-    default:
-      return state;
-  }
-}
-let initialState = { num: 0 };
-let store = createStore(reducer, initialState);
-let root = document.getElementById("root");
-let incrementBtn = document.getElementById("increment-btn");
-let decrementBtn = document.getElementById("decrement-btn");
-
-const render = () => {
-  root.innerHTML = store.getState().num;
-};
-render();
-store.subscribe(render);
-incrementBtn.addEventListener("click", () => {
-  store.dispatch({ type: INCREMENT });
-});
-decrementBtn.addEventListener("click", () => {
-  store.dispatch({ type: DECREMENT });
-});
+ReactDOM.render(<Counter />, document.getElementById("root"));
---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+import * as TYPES from "./action-types";
+
+let initialState = { num: 0 };
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TYPES.INCREMENT:
+      return { num: state.num + 1 };
+    case TYPES.DECREMENT:
+      return { num: state.num - 1 };
+    default:
+      return state;
+  }
+}
